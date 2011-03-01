3755a6e6de2f7fd59ae3be879b6c67f625e1bf7e
rowen <rowen@b00c9580-4927-0410-8824-811d448ea4c7>
2011-03-01 20:53:31 +0000

    Version 1.1rc1
    Modifications include:
    - Handles plFanuc files in inches or mm or a mix
    - A clearer error message if the config file is missing
    - Removed "Unadjusted" from the plFanuc file name, if founc
    - Processes all X,Y positions found, even if no after G60

--- python/adjustFanucFiles/adjustFanucFiles.py
+++ python/adjustFanucFiles/adjustFanucFiles.py
@@ -21,7 +21,7 @@ import RO.StringUtil
 import RO.Wdg
 import fitPlugPlateMeas.fitData as fitData
 
-__version__ = "1.0"
+__version__ = "1.1rc1"
 
 plateIDRE = re.compile(r"^Plug Plate: ([0-9a-zA-Z_]+) *(?:#.*)?$", re.IGNORECASE)
 measDateRE = re.compile(r"^Date: ([0-9-]+) *(?:#.*)?$", re.IGNORECASE)
@@ -50,14 +50,17 @@ class AdjustFanucFilesWdg(RO.Wdg.DropletApp):
             font = "Courier 12", # want a fixed width font
         )
         
-        self.numDig = 4 # number of digits after the decimal point for adjusted positions
+        self.numDig = None # number of digits after decimal point for adjusted positions: 3 for mm, 4 for inches
 
         self.logWdg.addMsg("""Adjust Fanuc Files version %s""" % (__version__,))
         
         homeDir = RO.OS.getHomeDir()
         config = ConfigParser.RawConfigParser()
         configPath = os.path.join(homeDir, ".adjustFanucFiles.dat")
+        self.model = None
         try:
+            if not os.path.isfile(configPath):
+                raise RuntimeError("file not found")
             config.read(configPath)
             qpMag = config.getfloat("quadrupole", "mag")
             qpAngle = config.getfloat("quadrupole", "angle")
@@ -69,9 +72,12 @@ class AdjustFanucFilesWdg(RO.Wdg.DropletApp):
             self.model.setMagnitudeAngle(qpMag, qpAngle)
         except Exception, e:
             self.logWdg.addMsg("Error reading %s: %s" % (configPath, RO.StringUtil.strFromException(e)), severity=RO.Constants.sevError)
-            self.logWdg.addMsg("Please quit, fix the config file and try again")
+            self.logWdg.addMsg("Please quit, fix the config file and try again", severity=RO.Constants.sevError)
 
-        self.lineRE = re.compile(r"^(.* )?G60 X([-0-9.]+) +Y([-0-9.]+)( .*)?$")
+        self.fileNameSubRE = re.compile("(.*)unadjusted(.*)", re.IGNORECASE)
+        self.inchesRE = re.compile(r"^[^(]*\bG20\b")
+        self.mmRE     = re.compile(r"^[^(]*\bG21\b")
+        self.xyPosRE  = re.compile(r"^(.* )X([-0-9.]+) +Y([-0-9.]+)( .*)?$")
             
         if filePathList:
             self.processFileList(filePathList)
@@ -82,37 +88,55 @@ class AdjustFanucFilesWdg(RO.Wdg.DropletApp):
         Convert lines that look like this:
         ...G60 X... Y... ...
         """
+        if not self.model:
+            return
         fileDir, fileName = os.path.split(filePath)
         
         baseName, ext = os.path.splitext(fileName)
         if ext.lower() != ".par":
             raise RuntimeError("Filename must end with .par")
-        outFileName = "%sAdjusted%s" % (baseName, ext)
+        
+        # output name = input name - "Unadjusted" + "Adjusted"
+        outBaseName = self.fileNameSubRE.sub(r"\1\2", baseName)
+        outFileName = "%sAdjusted%s" % (outBaseName, ext)
         outFilePath = os.path.join(fileDir, outFileName)
         
         qpMag, qpAngle = self.model.getMagnitudeAngle()
-            
+
+        # process the file by first reading in all the original data
+        # and accumulating the output, then write it all at once;
+        # that way if there is an error in the input file, no output file is written
         self.logWdg.addMsg("Converting %s to %s" % (fileName, outFileName))
         adjComment = "(Adjusted qpMag=%s qpAngle=%s)\n" % (qpMag, qpAngle,)
         lineNum = 0
+        outDataList = []
         with file(filePath, "rU") as inFile:
-            with file(outFilePath, "w") as outFile:
-                numAdj = 0
-                for line in inFile:
-                    lineNum += 1
-                    if lineNum == 3:
-                        outFile.write(adjComment)
-                    if line.lower().startswith("(adjusted"):
-                        raise RuntimeError("File has already been adjusted!")
-                    match = self.lineRE.match(line)
-                    if match:
-                        prefix, xPos, yPos, postfix = match.groups("")
-                        adjXYPos = self.model.applyOne([xPos, yPos], doInverse=True)
-                        outFile.write("%sG60 X%0.*f Y%0.*f%s\n" % \
-                            (prefix, self.numDig, adjXYPos[0], self.numDig, adjXYPos[1], postfix))
-                        numAdj += 1
-                    else:
-                        outFile.write(line)
+            numAdj = 0
+            for line in inFile:
+                lineNum += 1
+                if lineNum == 3:
+                    outDataList.append(adjComment)
+                if line.lower().startswith("(adjusted"):
+                    raise RuntimeError("File has already been adjusted!")
+                if self.mmRE.match(line):
+                    self.numDig = 3
+                elif self.inchesRE.match(line):
+                    self.numDig = 4
+                match = self.xyPosRE.match(line)
+                if match:
+                    if not self.numDig:
+                        raise RuntimeError("Cannot write modified x,y position: have not seen G20 or G21")
+                    prefix, xPos, yPos, postfix = match.groups("")
+                    adjXYPos = self.model.applyOne([xPos, yPos], doInverse=True)
+                    outDataList.append("%sX%0.*f Y%0.*f%s\n" % \
+                        (prefix, self.numDig, adjXYPos[0], self.numDig, adjXYPos[1], postfix))
+                    numAdj += 1
+                else:
+                    outDataList.append(line)
+        with file(outFilePath, "w") as outFile:
+            for line in outDataList:
+                outFile.write(line)
+        
         self.logWdg.addMsg("    adjusted %s x,y positions" % (numAdj,))
 
 if __name__ == "__main__":
